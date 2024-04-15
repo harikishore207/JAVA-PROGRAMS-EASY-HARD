f64b29e92194d66e55a19d90158b4b30d06186a5
23mx207 <hari2002kishore@gmail.com>
2024-04-15 17:25:13 +0530

    Saving latest Updates with script

---
+++ gitScript.bat
@@ -0,0 +1,10 @@
+::::git pull origin main
+::::git remote add origin https://...
+::git remote add origin https://github.com/githubtvp/23MX21.git
+git pull origin main --allow-unrelated-histories
+git add .
+git commit -a -m "Saving latest Updates with script"
+git push origin main
+
+echo "Git commands executed successfully."
+cmd
